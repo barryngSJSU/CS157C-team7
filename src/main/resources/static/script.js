ed1b34a2c6b1baa6f8f8588d6e91aa2de9f8e8eb
barryngSJSU <78779266+barryngSJSU@users.noreply.github.com>
2021-07-28 21:53:14 -0700

    Javascript functions to call backend APIs

--- src/main/resources/static/script.js
+++ src/main/resources/static/script.js
@@ -15,3 +15,67 @@ function getListing(title){
 		return JSON.parse(http.responseText);
 	}
 }
+
+function getCurrentUser(){
+	const http = new XMLHttpRequest();
+	http.open("GET", "/username", false);
+	http.send();
+	if(http.status == 200){
+		return http.responseText;
+	}
+}
+
+function getFullName(email){
+	const http = new XMLHttpRequest();
+	http.open("GET", "/fullName" + email, false);
+	http.send();
+	if(http.status == 200){
+		return http.responseText;
+	}
+}
+
+function search(tag){
+	const http = new XMLHttpRequest();
+	http.open("GET", "/search"+ tag , false);
+	http.send();
+	if(http.status == 200){
+		return JSON.parse(http.responseText);
+	}
+}
+
+function getTags(listing){
+	const http = new XMLHttpRequest();
+	http.open("GET", "/tags"+ listing , false);
+	http.send();
+	if(http.status == 200){
+		return http.responseText;
+	}
+}
+
+function getSeller(email){
+	const http = new XMLHttpRequest();
+	http.open("GET", "/seller"+ email, false);
+	http.send();
+	if(http.status == 200){
+		return JSON.parse(http.responseText);
+	}
+}
+
+function getReviews(email){
+	const http = new XMLHttpRequest();
+	http.open("GET", "/reviews"+ email, false);
+	http.send();
+	if(http.status == 200){
+		return JSON.parse(http.responseText);
+	}
+}
+
+function getReview(title){
+	const http = new XMLHttpRequest();
+	http.open("GET", "/review"+ title, false);
+	http.send();
+	if(http.status == 200){
+		return JSON.parse(http.responseText);
+	}
+}
+
